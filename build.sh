3b25c76a392eb96b89e975bec158a135e6226d57
XLion <xlion@xlion.tw>
2024-10-07 03:36:12 +0800

    change to RustDesk repo

--- build.sh
+++ build.sh
@@ -11,40 +11,8 @@ mkdir -p "/var/opt"
 ln -s "/var/opt"  "/opt"
 
 
-# >>>>>>>>>> RustDesk
-
-# Define the repository and the tag you want to fetch
-REPO="rustdesk/rustdesk"
-TAG="nightly"  # Change this to any tag you want
-API_URL="https://api.github.com/repos/$REPO/releases/tags/$TAG"
-
-# Fetch the release data for the specified tag using curl
-RELEASE_DATA=$(curl -s "$API_URL")
-
-# Check if RELEASE_DATA is not empty
-if [ -z "$RELEASE_DATA" ]; then
-    echo "Failed to fetch release data. Please check your internet connection or the repository/tag name."
-    exit 1
-fi
-
-# Use jq to parse JSON data and find the asset URL
-RUSTDESK_URL=$(echo "$RELEASE_DATA" | jq -r '.assets[] | select(
-    (.name | contains("x86_64")) and
-    (.name | endswith(".rpm")) and
-    (.name | contains("suse") | not) and
-    (.name | contains("sciter") | not)
-) | .browser_download_url' | head -n 1)
-
-# Check if the asset URL was found
-if [ -z "$RUSTDESK_URL" ]; then
-    echo "No matching file found."
-else
-    echo "RUSTDESK_URL=\"$RUSTDESK_URL\""
-fi
-
-
-rpm-ostree install $RUSTDESK_URL
-# <<<<<<<<<< RustDesk
+# Add xlion-rustdesk-rpm-repo.repo to /etc/yum.repos.d/
+curl -fsSl https://xlionjuan.github.io/rustdesk-rpm-repo/nightly.repo | sudo tee /etc/yum.repos.d/xlion-rustdesk-rpm-repo.repo
 
 
 # Add cloudflare-warp.repo to /etc/yum.repos.d/
@@ -52,7 +20,7 @@ curl -fsSl https://pkg.cloudflareclient.com/cloudflare-warp-ascii.repo | tee /et
 
 
 # Install
-rpm-ostree install cloudflare-warp htop plasma-workspace-x11 bootc ntpd-rs
+rpm-ostree install cloudflare-warp htop plasma-workspace-x11 bootc ntpd-rs rustdesk
 
 ### Install packages
 
