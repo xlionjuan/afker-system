e343349f6df0cd3c2c26ac788ad7c50471ce528c
XLion <xlion@xlion.tw>
2024-12-18 22:28:30 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -48,9 +48,9 @@ systemctl enable ntpd-rs
 
 # CachyOS Kernel
 dnf5 -y remove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra zram-generator-defaults
-dnf5 copr enable -y bieszczaders/kernel-cachyos
+dnf5 copr enable -y bieszczaders/kernel-cachyos-lto
 dnf5 copr enable -y bieszczaders/kernel-cachyos-addons
-rpm-ostree install kernel-cachyos
+rpm-ostree install kernel-cachyos-lto kernel-cachyos-lto-devel-matched
 setsebool -P domain_kernel_load_modules on
 dnf5 -y install scx-scheds cachyos-settings uksmd
 systemctl enable scx.service
